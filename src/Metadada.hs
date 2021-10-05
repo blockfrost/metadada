6d821735f6668f5854f38d6d27c55a9d4781ca30
Richard Marko <srk@48.io>
2021-10-05 16:07:05 +0200

    create dir

--- src/Metadada.hs
+++ src/Metadada.hs
@@ -25,10 +25,15 @@ dir = do
   args <- System.Environment.getArgs
   case args of
     [inDir, outDir] -> do
+
+      System.Directory.createDirectoryIfMissing True outDir
+
       files <- System.Directory.listDirectory inDir
+
       forM_ files $ \f -> do
         fc <- BL.readFile (inDir </> f)
         case Aeson.eitherDecode @Meta fc of
           Left e -> error $ "Unable to decode: " ++ show e
           Right meta -> BL.writeFile (outDir </> f) (Aeson.encode meta)
+
     _otherwise -> error "Usage: metadada-convert-dir inDir outDir"
